d50363a98f88a319c86b0dcc40ae75cd1d4e90b7
Hyrum Van Slyke <hyrumevanslyke@gmail.com>
2023-10-01 13:29:41 -0400

    Need to dubug .map in OptionDisplay.js

--- src/Components/OptionDisplay.js
+++ src/Components/OptionDisplay.js
@@ -1,7 +1,20 @@
 import React from "react";
-
+import { useSelector } from "react-redux";
+import { selectPotentials } from "../redux/slices/potentialCountriesSlice";
 const OptionDisplay = () => {
-    return <div className="stack">OptionDisplay</div>;
+  let currentPotential = useSelector(selectPotentials);
+  console.log(currentPotential);
+  return (
+    <div className="stack">
+      {currentPotential.map((e, i) => {
+        return (
+          <h2 key={e.name.official} className="country-option">
+            {e.name.common}
+          </h2>// ask about why the key is important and why .map would not be working
+        );
+      })}
+    </div>
+  );
 };
 
 export default OptionDisplay;
