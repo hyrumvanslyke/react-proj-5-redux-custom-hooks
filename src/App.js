1e90c0c26be38591499f8898dac1c0537179ecc2
Hyrum Van Slyke <hyrumevanslyke@gmail.com>
2023-10-01 13:07:30 -0400

    Need to debugg useSelector

--- src/App.js
+++ src/App.js
@@ -2,8 +2,11 @@ import "./App.css";
 import Header from "./Components/Header";
 import MainDisplay from "./Components/MainDisplay";
 import OptionDisplay from "./Components/OptionDisplay";
-
+import { selectPotentials } from "./redux/slices/potentialCountriesSlice";
+import { UseSelector } from "react-redux";
 function App() {
+    let potentials  = UseSelector(selectPotentials)
+    console.log(potentials)
     return (
         <div className="App font-link">
             <Header />
